8a43dc0e85c0e25a6aa9d3a803a8ba39966a77be
Ebrahim0pakawala <pakawalae@gmail.com>
2025-11-03 17:30:01 +0530

    more changes

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,10 @@ import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 
+
+
 const app = express();
+app.get('/api/health', (_, res) => res.status(200).send('OK'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
